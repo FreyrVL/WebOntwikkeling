308d168691299e11a655d569a4225b26693cf55f
Freyr <mail@freyrvl.dev>
2026-05-22 18:09:53 +0000

    CSS: Install Tailwind + DaisyUI + Postcss + generate output.css

--- express/transformers/index.ts
+++ express/transformers/index.ts
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set('views', path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.set("port", process.env.PORT || 3000);
 
