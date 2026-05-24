3eebd5a8e1dc440650f0a719cdbf886d9af1d84c
Freyr <mail@freyrvl.dev>
2026-05-24 01:04:54 +0000

    Create sessionmiddleware to enforce authorization/protect paths

--- express/transformers/middleware/sessionMiddleware.ts
+++ express/transformers/middleware/sessionMiddleware.ts
@@ -1,11 +1,23 @@
 import { NextFunction, Request, Response } from "express";
 
 export function secureMiddleware(req: Request, res: Response, next: NextFunction) {
-    if (req.session.user) {
-        res.locals.user = req.session.user;
-        console.log(req.session.user);
-        next();
-    } else {
+    if (!req.session.user) {
         res.redirect("/login");
+        return;
     }
+    next();
 };
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+    if (!req.session.user) {
+        res.redirect("/login");
+        return;
+    }
+    next();
+}
+export function requireAdmin(req: Request, res: Response, next: NextFunction) {
+    if (req.session.user?.role !== "ADMIN") {
+        res.status(403).send("Forbidden");
+        return;
+    }
+    next();
+}
